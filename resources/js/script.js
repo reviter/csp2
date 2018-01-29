870e12ab14ac42f2853b63e2c0487b72e4b8e474
reviter <stephenecanciller@gmail.com>
2018-01-30 00:10:26 +0800

    Changes made in Capstone 2

--- resources/js/script.js
+++ resources/js/script.js
@@ -82,10 +82,4 @@ $(document).ready(function() {
         }        
     });
 
-    /* Maps */
-    new GMaps({
-        div: '.map',
-        lat: -12.043333,
-        lng: -77.028333
-      });
 });
